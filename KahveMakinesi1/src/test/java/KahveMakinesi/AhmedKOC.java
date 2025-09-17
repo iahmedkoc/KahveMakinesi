8504eb1aaa17a2322d0e2d2c0ef3f470d0fab738
iahmedkoc <i.ahmedkoc@gmail.com>
2025-09-17 16:14:29 +0300

    guncelleme test 1

--- KahveMakinesi1/src/test/java/KahveMakinesi/AhmedKOC.java
+++ KahveMakinesi1/src/test/java/KahveMakinesi/AhmedKOC.java
@@ -2,7 +2,7 @@ package KahveMakinesi;
 
 public class AhmedKOC {
     public static void main(String[] args) {
-        System.out.println("degisiklik ts1 branch");
+        System.out.println("Bu satir duzeltildi");
         System.out.println("bir daha bak");
     }
 }
