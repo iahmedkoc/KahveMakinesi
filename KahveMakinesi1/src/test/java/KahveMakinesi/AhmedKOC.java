217f426df1134a03e91d5aa1bdec25aebebe4f42
iahmedkoc <i.ahmedkoc@gmail.com>
2025-09-17 16:06:06 +0300

    olusturuldu degisiklik 2

--- KahveMakinesi1/src/test/java/KahveMakinesi/AhmedKOC.java
+++ KahveMakinesi1/src/test/java/KahveMakinesi/AhmedKOC.java
@@ -3,5 +3,6 @@ package KahveMakinesi;
 public class AhmedKOC {
     public static void main(String[] args) {
         System.out.println("degisiklik ts1 branch");
+        System.out.println("bir daha bak");
     }
 }
